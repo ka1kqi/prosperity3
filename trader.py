a890155bd2e2574c1033e8c36783baef2a55139f
Arthur Du <arthur.x.du@gmail.com>
2025-04-10 00:07:17 -0400

    fin

--- trader.py
+++ trader.py
@@ -36,6 +36,8 @@ class Trader:
                     d = stdev 
                     best_bid = max(list(order_depth.buy_orders.keys()))
                     best_ask = min(list(order_depth.sell_orders.keys()))
+                    best_bid_amt = order_depth.buy_orders[best_bid]
+                    best_ask_amt = order_depth.sell_orders[best_ask]
                     spread = best_ask - best_bid
                     p=0
                     if product in state.position.keys():
@@ -43,19 +45,36 @@ class Trader:
                     if vwap > sma+d:
                         #-p-limit for sell
                         #limit-p for buy
-                        orders.append(Order(product,best_bid,-p-limit))
-                        print("sold squid_ink for: "+str(best_bid))
+                        orders.append(Order(product,int(vwap+1),-7)) #sell
                     if vwap < sma-d:
-                        orders.append(Order(product,best_ask,limit-p))
-                        print("bought squid_ink for: "+str(best_ask))
-                
-            
+                        orders.append(Order(product,int(vwap-1),7)) #buy
+                        
             if product == "KELP":
                 print()
 
             if product == "RAINFOREST_RESIN":
-                print()
-            
+                if product in state.position.keys():
+                    p=state.position[product]
+                else:
+                    p=0
+                d = .5
+                arb = False
+                limit=50
+                if len(order_depth.sell_orders) !=0:
+                    for ask, q in list(order_depth.sell_orders.items()):
+                        if ask < 10000:
+                            orders.append(Order(product,int(ask),-q))
+                            arb = True
+                        elif not arb and ask > 10000 + d:
+                            orders.append(Order(product,int(ask-d),-7))
+
+                if len(order_depth.buy_orders) !=0:
+                    for bid, q in list(order_depth.buy_orders.items()):
+                        if bid > 10000:
+                            orders.append(Order(product,int(bid),-q))
+                        elif not arb and bid < 10000-d:
+                            orders.append(Order(product,int(bid+d),7))
+        
             result[product] = orders
     
     
